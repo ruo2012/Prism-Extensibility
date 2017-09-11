3f49b603f49269e6679b9503076e6898d58fa74c
Dan Siegel <me@dansiegel.net>
2017-09-11 08:57:15 -0700

    bumping version for patch release

--- TemplatePack-XamarinStudio/TemplatePack-XamarinStudio/Properties/AddinInfo.cs
+++ TemplatePack-XamarinStudio/TemplatePack-XamarinStudio/Properties/AddinInfo.cs
@@ -2,7 +2,7 @@
 using Mono.Addins;
 using Mono.Addins.Description;
 
-[assembly:Addin ("PrismTemplatePack", Namespace = "Prism.Extensibility", Version = "1.6.0")]
+[assembly: Addin("PrismTemplatePack", Namespace = "Prism.Extensibility", Version = "1.6.1")]
 
 [assembly:AddinName ("Prism Template Pack")]
 [assembly:AddinCategory ("IDE extensions")]
