15919ca0d8a6eea954e37c413f8ab600b204c267
Brian Lagunas <brian.lagunas@live.com>
2017-10-28 02:08:19 -0600

    added XF module template

---
+++ TemplatePack/Prism.VisualStudio.Wizards/ModuleNameWizard.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TemplateWizard;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using EnvDTE;
+
+namespace Prism.VisualStudio.Wizards
+{
+    public class ModuleNameWizard : IWizard
+    {
+        public void BeforeOpeningFile(ProjectItem projectItem)
+        {
+            
+        }
+
+        public void ProjectFinishedGenerating(Project project)
+        {
+            
+        }
+
+        public void ProjectItemFinishedGenerating(ProjectItem projectItem)
+        {
+            
+        }
+
+        public void RunFinished()
+        {
+            
+        }
+
+        public void RunStarted(object automationObject, Dictionary<string, string> replacementsDictionary, WizardRunKind runKind, object[] customParams)
+        {
+            var projectName = replacementsDictionary["$projectname$"];
+            var moduleName = String.Format("{0}{1}", projectName.Split('.').Last(), "Module");
+            replacementsDictionary.Add("passthrough:ModuleName", moduleName);
+        }
+
+        public bool ShouldAddProjectItem(string filePath)
+        {
+            return true;
+        }
+    }
+}
---
+++ Templates/Xamarin.Forms/Module.XF/ModuleName.cs
@@ -0,0 +1,72 @@
+﻿using Prism.Modularity;
+using Module.XF.Views;
+using Module.XF.ViewModels;
+#if (AutofacContainer)
+using Autofac;
+using Prism.Autofac;
+#endif
+#if (DryIocContainer)
+using DryIoc;
+using Prism.DryIoc;
+#endif
+#if (NinjectContainer)
+using Ninject;
+using Prism.Ninject;
+#endif
+#if (UnityContainer)
+using Microsoft.Practices.Unity;
+using Prism.Unity;
+#endif
+
+namespace Module.XF
+{
+    public class ModuleName : IModule
+    {
+#if (AutofacContainer)
+        public void Initialize() { }
+
+        public static void Initialize(ContainerBuilder builder)
+        {
+            builder.Register<IFooService, FooService>();
+
+            builder.RegisterTypeForNavigation<ViewA, ViewAViewModel>();
+        }
+#elseif (DryIocContainer)
+        private IContainer _container { get; }
+
+        public ModuleName(IContainer container)
+        {
+            _container = container;
+        }
+
+        public void Initialize()
+        {
+            _container.RegisterTypeForNavigation<ViewA, ViewAViewModel>();
+        }
+#elseif (NinjectContainer)
+        private IKernel _kernel { get; }
+
+        public ModuleName(IKernel kernel)
+        {
+            _kernel = kernel;
+        }
+
+        public void Initialize()
+        {
+            _kernel.RegisterTypeForNavigation<ViewA, ViewAViewModel>();
+        }
+#elseif (UnityContainer)
+        private IUnityContainer _container { get; }
+
+        public ModuleName(IUnityContainer container)
+        {
+            _container = container;
+        }
+
+        public void Initialize()
+        {
+            _container.RegisterTypeForNavigation<ViewA, ViewAViewModel>();
+        }
+#endif
+    }
+}
